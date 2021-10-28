396619afd1ee8b08d24f77569db49a3237817934
benish-khan <bebe@Benishs-MacBook-Pro.local>
2021-10-28 10:17:52 -0700

    update Bbform

--- src/components/bbform.js
+++ src/components/bbform.js
@@ -1,9 +1,1 @@
 import React from 'react';
-<form name="contact" method="POST" data-netlify="true">
-<p>
-    <label> Your Name: <input type="text" name="name" /></label>
-</p>
-<p>
-    <label>Your Email: <input type="email" name="email" /></label>
-</p>
-</form>
