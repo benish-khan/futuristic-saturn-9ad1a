62184b9cc8062af798c25f239b47ae4c0da1bd10
benish-khan <bebe@Benishs-MacBook-Pro.local>
2021-10-28 16:25:27 -0700

    use require for function

--- netlify/functions/form-handler.js
+++ netlify/functions/form-handler.js
@@ -1,4 +1,4 @@
-import { parse } from 'querystring'
+const querystring = require('querystring')
 const axios = require('axios');
 const mailChimpAPI = process.env.MAILCHIMP_API_KEY;
 const mailChimpListID = process.env.MAILCHIMP_LIST_ID;
@@ -10,7 +10,7 @@ exports.handler = (event, context, callback) => {
   try {
     body = JSON.parse(event.body)
   } catch (e) {
-    body = parse(event.body)
+    body = querystring.parse(event.body)
   }
 
   if (!body.email) {
