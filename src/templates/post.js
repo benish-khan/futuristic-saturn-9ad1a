681795b98846f329e3b73e19a67f2ac75abeb364
bebe <benish.khan@netlify.com>
2021-11-04 19:48:00 -0700

    Update to main branch post.js file

--- src/templates/post.js
+++ src/templates/post.js
@@ -11,6 +11,7 @@ import BlogPostTags from '../components/BlogPostTags';
 
 // this minimal GraphQL query ensures that when 'gatsby develop' is running,
 // any changes to content files are reflected in browser
+// this is an updated comment from the main branch. 
 export const query = graphql`
   query($url: String) {
     sitePage(path: {eq: $url}) {
